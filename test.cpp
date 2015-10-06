f2ddadc5478bf49a20ee398c6f6abe3cfff4cf61
Your Name <griseldavcardozo@gmail.com>
2015-10-05 22:20:49 -0300

    cambios en el triangle

--- test.cpp
+++ test.cpp
@@ -49,73 +49,73 @@ int main() {
     auto V[7] = G.insertVertex(7);
     auto V[8] = G.insertVertex(8);
 */
-//    G.add_edge(V[1],V[2]);
-//    G.add_edge(V[1],V[3]);
-//    G.add_edge(V[1],V[7]);
-//    G.add_edge(V[1],V[6]);
-//
-//    G.add_edge(V[2],V[6]);
-//
-//    G.add_edge(V[2],V[4]);
-//    G.add_edge(V[2],V[5]);
-//
-//    V.push_back(G.add_vertex(9, {V[3], V[4], V[5]}));
-//
-////    G.removeEdge(V[1],V[6]);
-////    G.remove_vertex(V[6]);
-////     G.invariante_representacion();
-//
-////    G.removeEdge(V[1],V[3]);
-////    G.add_edge(V[3],V[7]);
-//
-//
-////    for(auto it = V[2].begin(); it != V[2].end(); ++it){
-////        cout<< *it <<endl;
-////    }
-////
-//    G.dump(cout);
-//    for(auto& v : V) {
-//        cout << "recorriendo H(" << *v << "): ";
-//        for(auto z = G.H_begin(v); z != G.H_end(v); ++z) {
-//            cout << *z << ',';
-//        }
-//        cout << endl;
-//        cout << "recorriendo H(" << *v << ") al reves: ";
-//        for(auto z = G.H_end(v); z != G.H_begin(v); --z) {
-//            cout << *prev(z) << ',';
-//        }
-//        cout << endl;
-//    }
-////    auto x = G.H_end(V[1]);
-////    --x;
-////    cout << *x << endl;
-////    --x;
-////    cout << *x << endl;
-////    --x;
-////    cout << *x << endl;
-////    --x;
-////    cout << *x << endl;
-//
-//
-//
-//    for(auto& v : V) {
-//        cout << "recorriendo N(" << *v << "): ";
-//        auto z = G.N_begin(v);
-//        for(; z != G.N_end(v); ++z) {
-//            cout << *z << ',';
-//        }
-//        cout << endl;
-//        cout << "recorriendo N(" << *v << ") al reves: ";
-//        for(; z != G.N_begin(v); --z) {
-//            cout << *prev(z) << ',';
-//        }
-//        cout << endl;
-//    }
-//
-//
-//    cout << G;
+    G.add_edge(V[1],V[2]);
+    G.add_edge(V[1],V[3]);
+    G.add_edge(V[1],V[7]);
+    G.add_edge(V[1],V[6]);
 
-    print_vertices(G);
+    G.add_edge(V[2],V[6]);
+
+    G.add_edge(V[2],V[4]);
+    G.add_edge(V[2],V[5]);
+
+    V.push_back(G.add_vertex(9, {V[3], V[4], V[5]}));
+
+//    G.removeEdge(V[1],V[6]);
+//    G.remove_vertex(V[6]);
+//     G.invariante_representacion();
+
+//    G.removeEdge(V[1],V[3]);
+//    G.add_edge(V[3],V[7]);
+
+
+//    for(auto it = V[2].begin(); it != V[2].end(); ++it){
+//        cout<< *it <<endl;
+//    }
+//
+    G.dump(cout);
+    for(auto& v : V) {
+        cout << "recorriendo H(" << *v << "): ";
+        for(auto z = G.H_begin(v); z != G.H_end(v); ++z) {
+            cout << *z << ',';
+        }
+        cout << endl;
+        cout << "recorriendo H(" << *v << ") al reves: ";
+        for(auto z = G.H_end(v); z != G.H_begin(v); --z) {
+            cout << *prev(z) << ',';
+        }
+        cout << endl;
+    }
+//    auto x = G.H_end(V[1]);
+//    --x;
+//    cout << *x << endl;
+//    --x;
+//    cout << *x << endl;
+//    --x;
+//    cout << *x << endl;
+//    --x;
+//    cout << *x << endl;
+
+
+
+    for(auto& v : V) {
+        cout << "recorriendo N(" << *v << "): ";
+        auto z = G.N_begin(v);
+        for(; z != G.N_end(v); ++z) {
+            cout << *z << ',';
+        }
+        cout << endl;
+        cout << "recorriendo N(" << *v << ") al reves: ";
+        for(; z != G.N_begin(v); --z) {
+            cout << *prev(z) << ',';
+        }
+        cout << endl;
+    }
+
+
+    cout << G;
+
+
 
 
     return 0;
