180584fd42ea42a1fd1cf3a46c154ea17d0ece5e
Your Name <griseldavcardozo@gmail.com>
2015-11-06 22:57:40 -0300

    generador y documentacion actualizada

--- generador.cpp
+++ generador.cpp
@@ -1,61 +1,66 @@
-#include <iostream>
-#include <fstream>
-#include <vector>
-#include "Graph.h"
-#include "Log.h"
-
-using namespace std;
-using namespace tip;
-
-using IntGraph = tip::Graph<int>;
-
-int main () {
-fstream ficheroEntrada;
-char cant_vertices;
-char vertex;
-char dim;
-char vecino;
+    #include <iostream>
+    #include <fstream>
+    #include <vector>
+    #include "Graph.h"
+    #include "Log.h"
 
 
-IntGraph G;
-vector<IntGraph::const_vertex_iterator> V;
+
+    using namespace std;
+    using namespace tip;
+
+    using IntGraph = tip::Graph<int>;
+
+    int main () {
+    fstream ficheroEntrada;
+    char cant_vertices;
+    char vertex;
+    char dim;
+    char vecino;
 
 
-ficheroEntrada.open ("graph.txt", ios::in);
+    IntGraph G;
+    vector<IntGraph::const_vertex_iterator> V;
 
-if (ficheroEntrada.is_open()) {
 
-ficheroEntrada >> cant_vertices;
+    ficheroEntrada.open ("graph.txt", ios::in);
 
-//agregamos los vertices
-for(char i = '0'; i < cant_vertices; i++) {
+    if (ficheroEntrada.is_open()) {
+
+    ficheroEntrada >> cant_vertices;
+
+    //agregamos los vertices
+    for(char i = '0'; i < cant_vertices; i++) {
     ficheroEntrada >> vertex;
-    V.push_back(G.insertVertex( (int) vertex));
-    //V[(int) vertex] = G.insertVertex((int) vertex);
-
+    int v = vertex -'0'; // convierto el char en int
+    V.push_back(G.insertVertex(v));
     }
 
-while (! ficheroEntrada.eof() ) {
+    while (! ficheroEntrada.eof() ) { //mientras no termine el archivo
 
-// crearmos las aristas.
- ficheroEntrada >> dim;
- ficheroEntrada >> vertex;
- cout <<" vertice "<< vertex;
- for(char j = '0'; j < dim; j++){
+        //crearmos las aristas.
+        ficheroEntrada >> vertex;
+        int vertice = vertex -'0';
+        ficheroEntrada >> vecino;
+        int vec = vecino -'0';
 
-    ficheroEntrada >> vecino;
+        if(vertex != 'F'){ // porque imprime dos veces la ultima arista
+            G.add_edge(V[vertice], V[vec]);
+        }
+    }
 
-    cout << " vecino " <<vecino;
+    ficheroEntrada.close();
+    }
+    else cout << "Fichero inexistente" << endl;
 
- }
 
- cout <<"" <<endl;
+    cout << G;
 
-}
-ficheroEntrada.close();
-}
-else cout << "Fichero inexistente" << endl;
+//    auto T = triangles(G);
+//    for(auto t = T.begin(); t != T.end(); ++t) {
+//        cout << t->v->elem << " " << t->w->elem << " " << t->z->elem << endl;
+//    }
 
-cout << G;
-return 0;
-}
+
+    return 0;
+    }
--- generador.py
+++ generador.py
@@ -3,17 +3,17 @@ from __future__ import print_function
 G = nx.complete_graph(5)
 
 
-
-
 def imprimir_grafo(grafo):
-        print( len(grafo.nodes()), end="")
-        for i in grafo:
-                print(i, end="")
-        for x in grafo.nodes():
-                print( len(grafo.neighbors(x)), end="")
-		print(x, end="")
-               for w in grafo.neighbors(x):
-                       print(w, end="")
+     print( len(grafo.nodes()), end="")
+     for i in G.nodes():
+             print(i, end="")
+     for x in G.edges():
+             L = list(x)
+             for z in L:
+                     print(z, end="")
+
 
 imprimir_grafo(G)
+ 
+
 
